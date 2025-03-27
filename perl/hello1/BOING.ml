5e4a37cdb031d0c6c923bd823f325d1276140a51
Chet Murthy <chetsky@gmail.com>
2025-03-27 12:24:35 -0700

    meh

--- perl/hello1/BOING.ml
+++ perl/hello1/BOING.ml
@@ -3,17 +3,18 @@ let verbose = ref true
 
 let main () =
   let cmd = Array.to_list (Sys.argv) in
+  let prog = List.hd cmd in
   let cmd = List.tl cmd in
   let cmd = Filename.quote_command (List.hd cmd) (List.tl cmd) in
-  Fmt.(pf stderr "LAUNCH: command %s\n%!" cmd);
+  Fmt.(pf stderr "%s: command %s\n%!" prog cmd);
   let st = Unix.system cmd in
   begin match st with
     Unix.WEXITED 0 -> Ok ()
   | Unix.WEXITED n -> exit n
   | WSIGNALED n ->
-     failwith (Printf.sprintf "LAUNCH: command killed by signal %d" n)
+     failwith (Printf.sprintf "%s: command killed by signal %d" prog n)
   | WSTOPPED n ->
-     failwith (Printf.sprintf "LAUNCH: command stopped by signal %d" n)
+     failwith (Printf.sprintf "%s: command stopped by signal %d" prog n)
   end
 
 let _ =
